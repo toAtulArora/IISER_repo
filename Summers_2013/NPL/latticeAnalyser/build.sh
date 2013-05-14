85749c48d6c87e4ae693daff193ff5c6682fe577
root <root@ccf1.ccf.npl>
2013-05-14 12:58:47 -0800

    Up and running in the new setup

--- Summers_2013/NPL/latticeAnalyser/build.sh
+++ Summers_2013/NPL/latticeAnalyser/build.sh
@@ -1,2 +1,3 @@
-export PKG_CONFIG_PATH='/media/atul/a4c78183-78c2-4a82-9985-21937168d57f/Summer13/opencv/release2/unix-install'
+#export PKG_CONFIG_PATH='/media/atul/a4c78183-78c2-4a82-9985-21937168d57f/Summer13/opencv/release2/unix-install'
+export LD_LIBRARY_PATH=/usr/local/src/opencv-2.4.5/release/lib
 g++ -ggdb `pkg-config --cflags opencv` -o latticeAnalyser latticeAnalyser.cpp `pkg-config --libs opencv`
