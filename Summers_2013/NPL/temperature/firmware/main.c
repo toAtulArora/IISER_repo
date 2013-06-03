45870e0b999e2a2fb5262c1724524c32344146e0
Atul Singh Arora <toatularora@gmail.com>
2013-06-03 15:11:22 +0530

    temperature: Brought to life

---
+++ Summers_2013/NPL/temperature/firmware/main.c
@@ -0,0 +1,34 @@
+#include "usbIO.h"
+
+int main(void)
+{
+	U8Bit   i;
+	U8Bit   ucInDataLen;
+	U8Bit   *pucInData = 0;
+
+	usbInit();
+
+	// ---------------- enforce re-enumeration.
+	usbDeviceDisconnect();  // enforce re-enumeration, do this while interrupts are disabled!
+	i = 255;
+	while(--i)					// fake USB disconnect for > 250 ms
+		_delay_ms(1);
+	usbDeviceConnect();
+
+	sei();
+	for(;;)
+	{
+		usbPoll();
+		ucInDataLen = ucGetUSBData( &pucInData );
+		if ( ucInDataLen > INBUFFER_LEN )
+			ucInDataLen = INBUFFER_LEN;
+		if ( ucInDataLen )
+		{						// copy indata and send back ascii values incremented by 1
+			pucOutBuf[0] = ucInDataLen;
+			for ( i = 0; i < ucInDataLen; i++ )
+				pucOutBuf[i+1] = pucInData[i] + 1;
+			vSendUSBData( ucInDataLen+1 );
+		}
+	}
+	return 0;
+}
