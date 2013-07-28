0b47fd02286123ed3af8f86061584b61eca3bde7
Atul Sinh Aurora <toatularora@gmail.com>
2013-07-29 00:22:14 +0530

    Improved the UI and added functionality to save, rather naiive at this stage

--- Summers_2013/nazarBand/pieces/faceDetectAndTrain.cpp
+++ Summers_2013/nazarBand/pieces/faceDetectAndTrain.cpp
@@ -20,15 +20,18 @@ void getMidPoint(Rect r,float& x, float& y)
 
 int main(int argc, const char *argv[])
 {
-	string windowName="Camera Stream",debugWindow="Gray Scale",candidateWindow="Candidate";
+	string windowName="Camera Stream",debugWindow="Gray-Scale",candidateWindow="Candidate Preview";
 	namedWindow(windowName,WINDOW_AUTOSIZE);
 	namedWindow(debugWindow,WINDOW_AUTOSIZE);
 	namedWindow(candidateWindow,WINDOW_AUTOSIZE);
 	Mat frame,frameClone,gray,roi,visRoi,trainRoi,visTrainRoi;
 	Mat rotMat(2,3,CV_32FC1);
 
-	bool getCandidate=false;
+	bool getCandidate=false,candidateInBuffer=false;
 	
+	//Settings
+	bool waitForEyes=true;
+
 	string cascadeFn = "c:/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
 	string nestedFn = "c:/opencv/data/haarcascades/haarcascade_eye.xml";
 	//CascadeClassifier cascade(cascadeFn), nestedCascade(nestedFn);
@@ -63,32 +66,60 @@ int main(int argc, const char *argv[])
 			roi=gray(r);
 			nestedCascade.detectMultiScale(roi,subrects,1.3,4,CV_HAAR_SCALE_IMAGE,Size(r.width/5,r.height/5),Size(r.width/2.5,r.height/2.5));
 			rectangle(frame,r,Scalar(255,0,0));
+
 			//visRoi=frame(r);
 			for(Rect rr: subrects)
 				rectangle(frame(r),(rr),Scalar(0,255,0));
-			if(subrects.size()==2 && getCandidate==true)
+			
+			
+			if(getCandidate)
 			{
-				getCandidate=false;
-				float x1,y1,x2,y2;
-				getMidPoint(subrects[0],x1,y1);
-				getMidPoint(subrects[1],x2,y2);				
-				float angle=atan2(y2-y1,x2-x1)*(180.0/PI);
-				if(x1>x2)
-					angle-=180;
-				//angle=-angle;
-				Point center=Point((x1+x2)/2,(y1+y2)/2);
-				rotMat=getRotationMatrix2D(center,angle,1.15);
-				cout<<angle<<endl;
-				//trainRoi=frameClone(r);				
-				warpAffine(frame(r),visTrainRoi,rotMat,frame(r).size());
-				warpAffine(frameClone(r),trainRoi,rotMat,frameClone(r).size());
-				imshow(candidateWindow,visTrainRoi);
+				//This may be slightly unclear, but when you want to acquire another candidate, then 
+				//save the previous one, if there was any
+				//Another key can be used to reject
+				if(candidateInBuffer)
+				{
+					static int count=0;
+					count++;
+					char filename[10];
+					vector<int> compression_params;
+					compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+					compression_params.push_back(9);
+					sprintf(filename,"%d.png",count);
+					imwrite(filename,trainRoi,compression_params);
+					candidateInBuffer=false;	//Saved!
+				}
+				if(!waitForEyes)
+				{
+					getCandidate=false;
+					Point center=Point(frame(r).cols/2,frame(r).rows/2);
+					rotMat=getRotationMatrix2D(center,0,1.15);
+					warpAffine(frame(r),visTrainRoi,rotMat,frame(r).size());
+					warpAffine(frameClone(r),trainRoi,rotMat,frame(r).size());
+					imshow(candidateWindow,visTrainRoi);
+					candidateInBuffer=true;
+				}
+				else if(subrects.size()==2 && waitForEyes==true)
+				{
+					getCandidate=false;
+					float x1,y1,x2,y2;
+					getMidPoint(subrects[0],x1,y1);
+					getMidPoint(subrects[1],x2,y2);				
+					float angle=atan2(y2-y1,x2-x1)*(180.0/PI);
+					if(x1>x2)
+						angle-=180;
+					//angle=-angle;
+					Point center=Point((x1+x2)/2,(y1+y2)/2);
+					rotMat=getRotationMatrix2D(center,angle,1.15);
+					cout<<angle<<endl;						
+					warpAffine(frame(r),visTrainRoi,rotMat,frame(r).size());
+					warpAffine(frameClone(r),trainRoi,rotMat,frameClone(r).size());
+					imshow(candidateWindow,visTrainRoi);
+					candidateInBuffer=true;
+				}
+
 			}
 		}		
-	// vector<int> compression_params;
-    // compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    // compression_params.push_back(9);
-	// imwrite("alpha.png",mat,compression_params);
 		imshow(windowName,frame);
 		imshow(debugWindow,gray);
 		char key=(char) waitKey(1);
@@ -97,8 +128,16 @@ int main(int argc, const char *argv[])
 			case 'q':
 			case 'Q':
 				return 0;
+			case 'e':
+			case 'E':
+				waitForEyes=!waitForEyes;
+				break;
+			case 'r':
+			case 'R':	//Reject candidate
+				candidateInBuffer=false;
+				break;
 			case 'c':
-			case 'C':
+			case 'C':	//Capture and show a candidate (implied, save the last one)
 				getCandidate=true;
 				break;
 			default:
