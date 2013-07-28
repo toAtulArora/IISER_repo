11edd3e15bf50f58204531cd23e2cf078ab77a68
Atul Sinh Aurora <toatularora@gmail.com>
2013-07-28 22:36:40 +0530

    Completed face detection and saving

---
+++ Summers_2013/nazarBand/pieces/faceDetectAndTrain.cpp
@@ -0,0 +1,107 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+
+#include <iostream>
+#include <iterator>
+#include <cctype>
+#include <vector>
+#include <math.h>
+// #include <fstream>
+// #include <sstream>
+#define PI 3.14159265
+using namespace cv;
+using namespace std;
+void getMidPoint(Rect r,float& x, float& y)
+{
+	x=r.x + (r.width/2);
+	y=r.y + (r.height/2);	
+}
+
+int main(int argc, const char *argv[])
+{
+	string windowName="Camera Stream",debugWindow="Gray Scale",candidateWindow="Candidate";
+	namedWindow(windowName,WINDOW_AUTOSIZE);
+	namedWindow(debugWindow,WINDOW_AUTOSIZE);
+	namedWindow(candidateWindow,WINDOW_AUTOSIZE);
+	Mat frame,frameClone,gray,roi,visRoi,trainRoi;
+	Mat rotMat(2,3,CV_32FC1);
+
+	bool getCandidate=false;
+	
+	string cascadeFn = "c:/opencv/data/haarcascades/haarcascade_frontalface_alt.xml";
+	string nestedFn = "c:/opencv/data/haarcascades/haarcascade_eye.xml";
+	//CascadeClassifier cascade(cascadeFn), nestedCascade(nestedFn);
+	CascadeClassifier cascade,nestedCascade;
+	if(!cascade.load(cascadeFn))
+		cout<<"Couldn't open the cascade file"<<endl<<cascadeFn;
+	if(!nestedCascade.load(nestedFn))
+		cout<<"Couldn't open the cascade for the eyes"<<endl<<nestedFn;
+
+	// CascadeClassifier(cascadeFn);
+	
+	// video source = 0 (default camera)
+	VideoCapture capture(0);
+	if(!capture.isOpened())
+		cout<<"Couldn't read from the camera";
+	
+	for(;;)
+	{
+		capture>>frame;
+		frameClone=frame.clone();
+		if(frame.empty())
+			cout<<"Can't read from the stream";					
+		
+		cvtColor(frame,gray,COLOR_BGR2GRAY);
+		equalizeHist(gray,gray);
+		vector<Rect> rects,subrects;
+		
+		// CascadeClassifier::detectMultiScale(const Mat& image, vector<Rect>& objects, double scaleFactor=1.1, int minNeighbors=3, int flags=0, Size minSize=Size(), Size maxSize=Size())		
+		cascade.detectMultiScale(gray,rects,1.3,4,CV_HAAR_SCALE_IMAGE,Size(30,30),Size(300,300));		
+		for(Rect r: rects)
+		{
+			roi=gray(r);
+			nestedCascade.detectMultiScale(roi,subrects,1.3,4,CV_HAAR_SCALE_IMAGE,Size(30,30),Size(300,300));
+			rectangle(frame,r,Scalar(255,0,0));
+			visRoi=frame(r);
+			for(Rect rr: subrects)
+				rectangle(visRoi,(rr),Scalar(0,255,0));
+			if(subrects.size()==2 && getCandidate==true)
+			{
+				getCandidate=false;
+				float x1,y1,x2,y2;
+				getMidPoint(subrects[0],x1,y1);
+				getMidPoint(subrects[1],x2,y2);				
+				float angle=atan2(y2-y1,x2-x1)*(180.0/PI);
+				if(x1>x2)
+					angle-=180;
+				//angle=-angle;
+				Point center=Point((x1+x2)/2,(y1+y2)/2);
+				rotMat=getRotationMatrix2D(center,angle,1.2);
+				cout<<angle<<endl;
+				// trainRoi=frameClone(r);				
+				warpAffine(visRoi,trainRoi,rotMat,visRoi.size());
+				imshow(candidateWindow,trainRoi);
+			}
+		}		
+	// vector<int> compression_params;
+    // compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+    // compression_params.push_back(9);
+	// imwrite("alpha.png",mat,compression_params);
+		imshow(windowName,frame);
+		imshow(debugWindow,gray);
+		char key=(char) waitKey(1);
+		switch(key)
+		{
+			case 'q':
+			case 'Q':
+				return 0;
+			case 'c':
+			case 'C':
+				getCandidate=true;
+				break;
+			default:
+			break;
+		}		
+	}
+}
